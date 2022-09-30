c5c6518dd32876237b057db52cc6d572c3c4c956
Artur <archibay16@gmail.com>
2022-09-30 05:44:35 +0300

    second task is completed

--- main.py
+++ main.py
@@ -9,9 +9,15 @@
 #     assert parse('http://example.com/?') == {}
 #     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
+from http.cookies import SimpleCookie
+
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    cookie = SimpleCookie()
+    cookie.load(query)
+    res = {k: v.value for k, v in cookie.items()}
+    print(res)
+    return res
 
 
 if __name__ == '__main__':
@@ -19,3 +25,12 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('1=1;1=1;') == {'1': '1'}
+    assert parse_cookie('name;age=28;') == {}
+    assert parse_cookie('name=Dima;age=28;gender=male') == {'name': 'Dima', 'age': '28', 'gender': 'male'}
+    assert parse_cookie('!=!;') == {'!': '!'}
+    assert parse_cookie('=Dima') == {}
+    assert parse_cookie('.=.') == {'.': '.'}
+    assert parse_cookie('1=1;1=2;1=3;1=4') == {'1': '4'}
+    assert parse_cookie('name=Dima;;;;;;;;;;;;') == {'name': 'Dima'}
+    assert parse_cookie('FSDFSDFSDF=SDFSDFSDF') == {'FSDFSDFSDF': 'SDFSDFSDF'}
